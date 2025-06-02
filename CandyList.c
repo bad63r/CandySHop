693665640d53a8926de85c1f490563879687112e
Tomislav Pap <bad63r@gmail.com>
2025-06-02 19:55:27 +0200

    adding candy list

---
+++ CandyList.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+  printf("Candy List:\n");
+  printf("* Chocolate candy njam njam!\n");
+  printf("* Caramela candy with soo much sugar grrr\n");
+  printf("* Apple simple candy\n");
+  printf("* vegan candy for hippy ppl :o)");
+  
+  
+  return 0;
+}
