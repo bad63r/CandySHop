d0c27a9b25e44189496c51b10bb1960a95e7eedf
Tomislav Pap <bad63r@gmail.com>
2025-06-02 20:18:44 +0200

    adding color recommendation

--- CandyList.c
+++ CandyList.c
@@ -2,12 +2,25 @@
 
 int main(int argc, char *argv[])
 {
+  enum colors {
+    blue,
+    white,
+    yellow,
+    red,
+    black
+  };
 
   printf("Candy List:\n");
   printf("* Chocolate candy njam njam!\n");
   printf("* Caramela candy with soo much sugar grrr\n");
   printf("* Apple simple candy\n");
-  printf("* vegan candy for hippy ppl :o)");
+  printf("* vegan candy for hippy ppl :o) \n\n\n");
+
+  printf("we have candy with different colors:\n");
+
+  enum colors colorCandy = yellow;
+  if (colorCandy == yellow) printf("the best color we think is yellow\n");
+  
   
   
   return 0;
